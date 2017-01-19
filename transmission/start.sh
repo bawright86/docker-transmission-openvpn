a163ee9e9cca15902ac0f6a5e8b1f4509c88b75b
bwright86 <bwright86@github.com>
2017-01-19 19:19:05 +1000

    modify couchpotato startup 	modified:   transmission/start.sh

--- transmission/start.sh
+++ transmission/start.sh
@@ -37,13 +37,13 @@ echo "Transmission startup script complete."
 
 # if the /data/couchpotato directory doesn't contain anything, then start CouchPotatoServer, stop it,
 # and update the settings so that the wizard doesn't run (which in the current version seems buggy).
-directory="/data/couchpotato"
+directory="/data/.config/couchpotato"
 if [ ! "$(ls -A $directory)" ]; then
     /usr/bin/python2.7 /opt/CouchPotatoServer/CouchPotato.py --data_dir=/data/.config/couchpotato --console_log & \
     sleep 10
     kill $(pidof python2.7)
     sleep 3
-    #sed -i "s/show_wizard = 1/show_wizard = 0/" /data/couchpotato/settings.conf 
+    sed -i "s/show_wizard = 1/show_wizard = 0/" /data/.config/couchpotato/settings.conf 
     /usr/bin/python2.7 /opt/CouchPotatoServer/CouchPotato.py --data_dir=/data/.config/couchpotato --console_log &
 else
     /usr/bin/python2.7 /opt/CouchPotatoServer/CouchPotato.py --data_dir=/data/.config/couchpotato --console_log &
@@ -52,7 +52,7 @@ fi
 echo "CouchPotato startup script complete."
 # if the /data/couchpotato directory doesn't contain anything, then start CouchPotatoServer, stop it,
 # and update the settings so that the wizard doesn't run (which in the current version seems buggy).
-directory="/data/sickrage"
+directory="/data/.config/sickrage"
 if [ ! "$(ls -A $directory)" ]; then
     /usr/bin/python2.7 /opt/SickRage/SickBeard.py --datadir=/data/.config/sickrage & \
     sleep 10
